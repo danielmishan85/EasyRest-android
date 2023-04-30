ceab3836aba2ed93fdbb21ee5fd761b5b1db155a
lital.hekmati <litalhechmati@gmail.com>
2023-04-30 23:24:04 +0300

    connect to the server successfully

--- app/src/main/java/com/example/easyrestapp/StartActivity.java
+++ app/src/main/java/com/example/easyrestapp/StartActivity.java
@@ -1,16 +1,55 @@
 package com.example.easyrestapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
-
-import com.example.easyrestapp.model.ServerConnection;
+import java.io.IOException;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class StartActivity extends AppCompatActivity {
 
-    ServerConnection serverConnection;
+    //------------------getByCategory-----------------------//
+    //POST EXAMPLE
+    /*
+    public String postUrl= "http://10.0.2.2:3001/dish/getByCategory";
+    public String postBody="{\n" +
+            "    \"dishCategory\": \"starter\"\n" +
+            "}";
+
+     */
+
+    //------------------addToOrder-----------------------//
+    //POST EXAMPLE
+
+    public String postUrl= "http://10.0.2.2:3001/openTable/addToOrder";
+    public String postBody = "{\n" +
+            "    \"tableId\": \"644ecbee5062641bd0352946\",\n" +
+            "    \"dishArray\": [\n" +
+            "        {\n" +
+            "            \"dishid\": \"643ef91662cdc37f5379e502\",\n" +
+            "            \"amount\": 1,\n" +
+            "            \"firstOrMain\": \"F\",\n" +
+            "            \"allTogether\": true\n" +
+            "        }\n" +
+            "    ]\n" +
+            "}";
+
+    //------------------categoryList-----------------------//
+    //GET EXAMPLE
+    //public String postUrl= "http://10.0.2.2:3001/dish/categoryList";
+
+
+
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,18 +58,72 @@ public class StartActivity extends AppCompatActivity {
         Button waiter_btn = this.findViewById(R.id.waiter_btn);
 
         waiter_btn.setOnClickListener((v)->{
-            serverConnection.getDishesByCategory("starter","MyRest");
             Intent i= new Intent(this,WaiterActivity.class);
             startActivity(i);
 
         });
 
         kitchen_btn.setOnClickListener((v)->{
-            serverConnection = new ServerConnection("5.5.5.5", 3001);
+            try {
+                postRequest(postUrl,postBody);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
             Intent i= new Intent(this,KitchenActivity.class);
             startActivity(i);
         });
 
+    }
+
+    void postRequest(String postUrl,String postBody) throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(JSON, postBody);
+
+        Request request = new Request.Builder()
+                .url(postUrl)
+                .post(body)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.d("server", e.getMessage() );
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d("server", "litalllll" + response.body().string());
+                Log.d("server","litalllll" + response + "");
+                Log.d("server","litalllll" + response.body() + "");
+            }
+        });
+    }
+
+    void getRequest(String postUrl) throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(postUrl)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.d("server", e.getMessage() );
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d("server", "get" + response.body().string());
+                Log.d("server","get" + response + "");
+                Log.d("server","fet" + response.body() + "");
+            }
+        });
     }
 }
--- app/src/main/java/com/example/easyrestapp/model/ServerConnection.java
+++ app/src/main/java/com/example/easyrestapp/model/ServerConnection.java
@@ -1,94 +1,93 @@
-package com.example.easyrestapp.model;
-
-import android.util.Log;
-import com.google.gson.Gson;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-public class ServerConnection {
-    private ExecutorService executorService;
-    private String serverAddress;
-    private int serverPort;
-    private HttpURLConnection connection;
-    private Gson gson;
-
-    public ServerConnection(final String serverAddress, final int serverPort) {
-        this.serverAddress = serverAddress;
-        this.serverPort = serverPort;
-        executorService = Executors.newSingleThreadExecutor();
-        gson = new Gson();
-        executorService.execute(() -> {
-            try {
-                // Establish the connection with the server
-                connection = (HttpURLConnection) new URL("http://" + serverAddress + ":" + serverPort).openConnection();
-                Log.d("server", "Connection successful!");
-            } catch (IOException e) {
-                // Handle IO exception
-                Log.d("server", e + "");
-            }
-        });
-    }
-
-    public void getDishesByCategory(String category, String restaurantName) {
-        if (connection == null) {
-            Log.d("server", "Connection not established!");
-            return;
-        }
-        executorService.execute(() -> {
-            try {
-                // Construct request URL
-                URL url = new URL("http://127.0.0.1:3001/getByCategory");
-
-                // Open HTTP connection
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-                // Set request method and headers
-                connection.setRequestMethod("POST");
-                connection.setRequestProperty("Content-Type", "application/json");
-                connection.setDoOutput(true);
-
-                // Create request body
-                RequestBody requestBody = new RequestBody(category, restaurantName);
-                Gson gson = new Gson();
-                String requestBodyJson = gson.toJson(requestBody);
-
-                // Write request body to output stream
-                OutputStream outputStream = connection.getOutputStream();
-                outputStream.write(requestBodyJson.getBytes());
-                outputStream.flush();
-                outputStream.close();
-
-                // Read response from input stream
-                InputStream inputStream = connection.getInputStream();
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                String response = bufferedReader.readLine();
-                bufferedReader.close();
-                inputStream.close();
-
-                // Log response
-                Log.d("server", response);
-            } catch (IOException e) {
-                // Handle IO exception
-                Log.d("server", e + "");
-            }
-        });
-    }
-
-    private static class RequestBody {
-        private String dishCategory;
-        private String ResturantName;
-
-        public RequestBody(String dishCategory, String ResturantName) {
-            this.dishCategory = dishCategory;
-            this.ResturantName = ResturantName;
-        }
-    }
-
-}
+//package com.example.easyrestapp.model;
+//
+//import android.util.Log;
+//import com.google.gson.Gson;
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.io.InputStreamReader;
+//import java.io.OutputStream;
+//import java.net.HttpURLConnection;
+//import java.net.URL;
+//import java.util.concurrent.ExecutorService;
+//import java.util.concurrent.Executors;
+//
+//public class ServerConnection {
+//    private ExecutorService executorService;
+//    private String serverAddress;
+//    private int serverPort;
+//    private HttpURLConnection connection;
+//    private Gson gson;
+//
+//    public ServerConnection(final String serverAddress, final int serverPort) {
+//        this.serverAddress = serverAddress;
+//        this.serverPort = serverPort;
+//        executorService = Executors.newSingleThreadExecutor();
+//        gson = new Gson();
+//        executorService.execute(() -> {
+//            try {
+//                // Establish the connection with the server
+//                connection = (HttpURLConnection) new URL("http://" + serverAddress + ":" + serverPort).openConnection();
+//                Log.d("server", "Connection successful!");
+//            } catch (IOException e) {
+//                // Handle IO exception
+//                Log.d("server", e + "");
+//            }
+//        });
+//    }
+//
+//    public void getDishesByCategory(String dishCategory) {
+//        if (connection == null) {
+//            Log.d("server", "Connection not established!");
+//            return;
+//        }
+//        executorService.execute(() -> {
+//            try {
+//                // Construct request URL
+//                URL url = new URL("http://127.0.0.1:3001/dish/getByCategory");
+//
+//                // Open HTTP connection
+//                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//
+//                // Set request method and headers
+//                connection.setRequestMethod("POST");
+//                connection.setRequestProperty("Content-Type", "application/json");
+//                connection.setDoOutput(true);
+//
+//                // Create request body
+//                RequestBody requestBody = new RequestBody(dishCategory);
+//                Gson gson = new Gson();
+//                String requestBodyJson = gson.toJson(requestBody);
+//                Log.d("server", "GSON: " + requestBodyJson);
+//                // Write request body to output stream
+//                OutputStream outputStream = connection.getOutputStream();
+//                outputStream.write(requestBodyJson.getBytes());
+//                outputStream.flush();
+//                outputStream.close();
+//
+//                // Read response from input stream
+//                InputStream inputStream = connection.getInputStream();
+//                Log.d("server", "before reader: " + inputStream + "");
+//                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+//                String response = bufferedReader.readLine();
+//                bufferedReader.close();
+//                inputStream.close();
+//
+//                // Log response
+//                Log.d("server", "after reader: "+ response);
+//            } catch (IOException e) {
+//                // Handle IO exception
+//                Log.d("server", e + "");
+//            }
+//        });
+//    }
+//
+//    private static class RequestBody {
+//        private String dishCategory;
+//
+//        public RequestBody(String dishCategory) {
+//            this.dishCategory = dishCategory;
+//        }
+//    }
+//
+//}
